fe4cd4ac3cbae5d6daeb79c56feab039b2ec5bf5
stge35 <evanok1@gmail.com>
2023-06-01 14:00:27 +0900

    Add AppConfig

---
+++ src/main/java/basic/core/AppConfig.java
@@ -0,0 +1,20 @@
+package basic.core;
+
+import basic.core.discount.FixDiscountPolicy;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+import basic.core.member.MemoryMemberRepository;
+import basic.core.order.OrderService;
+import basic.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
