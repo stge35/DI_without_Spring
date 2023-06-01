e515e05987b2a7db3d393f0838ae6cd2ca900a61
stge35 <evanok1@gmail.com>
2023-06-01 13:39:18 +0900

    Add orderService Test

--- src/main/java/basic/core/discount/RateDiscountPolicy.java
+++ src/main/java/basic/core/discount/RateDiscountPolicy.java
@@ -1,4 +1,10 @@
 package basic.core.discount;
 
+import basic.core.member.Member;
+
 public class RateDiscountPolicy implements DiscountPolicy{
+    @Override
+    public int discount(Member member, int price) {
+        return 0;
+    }
 }
---
+++ src/test/java/basic/core/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package basic.core.order;
+
+import basic.core.member.Grade;
+import basic.core.member.Member;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
